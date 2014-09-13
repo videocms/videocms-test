d8d7ca42cdbc77a7de5181e16cde027c35ffaa9f
mykyy <mykyy@mykyy-desktop>
2014-09-13 11:54:42 +0200

    Zmiana hasła| do zmiany język

--- protected/models/CmsvideoChangePass.php
+++ protected/models/CmsvideoChangePass.php
@@ -19,5 +19,28 @@ class CmsvideoChangePass extends CFormModel
         );
     }
     
+    public function attributeLabels() {
+        return array(
+            'uzytkownik_haslo' => 'Stare Hasło',
+            'uzytkownik_nowehaslo' => 'Nowe hasło',
+            'uzytkownik_nowehaslo2' => 'Powtorz nowe hasło',
+        );
+    }
     
+    public function WybierzUzytkownika($Id)
+    {
+        $WybierzUzytkownikow = Yii::app()->db->createCommand('SELECT * FROM videocms_users WHERE uzytkownik_id =:UzytkownikId');
+        $WybierzUzytkownikow->bindValue(':UzytkownikId', $Id, PDO::PARAM_INT);
+        $DaneUzytkownicy = $WybierzUzytkownikow->queryAll();
+        
+        return $DaneUzytkownicy;
+    }
+    
+    public function AktualizujHaslo($Id)
+    {
+        $AktualizujHaslo = Yii::app()->db->createCommand('UPDATE vidocms_users SET uzytkownik_haslo = :UzytkownikHaslo WHERE uzytkownik_id = :UzytkownikId');
+        $AktualizujHaslo->bindValue(':UzytkownikHaslo', md5($this->uzytkownik_nowehaslo), PDO::PARAM_STR);
+        $AktualizujHaslo->bindValue(':UzytkownikId', $Id, PDO::PARAM_INT);
+        $AktualizujHaslo->execute();
+    }
 }
