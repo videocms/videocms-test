6c874c44a87a11c3dd077527d20b3c2188754f0b
mykyy <mykyy@mykyy-desktop>
2014-09-13 14:44:43 +0200

    English version CmsvideoUser

---
+++ protected/models/CmsvideoUsers.php
@@ -0,0 +1,48 @@
+<?php
+
+class CmsvideoUsers extends CFormModel
+{
+    public $user_login;
+    public $user_pass;
+
+
+    public function rules()
+    {
+        return array (
+            array('user_login, user_pass', 'required'),
+            array('user_login', 'length', 'max'=>150),
+            array('user_pass', 'length', 'max'=>50),
+        );
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+            'user_id' => 'ID',
+            'user_login' => 'Login',
+            'user_pass' => 'Hasło',
+        );
+    }
+#LiczIleUzytkownikow
+    public function CountHowManyUsers()
+    {
+        $SelectUsers = Yii::app()->db->createCommand('SELECT count(user_id) FROM videocms_users WHERE user_login = :UserLogin AND user_pass = :UserPass');
+        $SelectUsers->bindValue(':UserLogin', $this->user_login, PDO::PARAM_STR);
+        $SelectUsers->bindValue(':UserkPass', md5($this->user_pass), PDO::PARAM_STR);
+        $DataUsers = $SelectUsers->queryScalar();
+
+        return $DataUsers;
+    }
+
+    public function SelectUser()
+    {
+        $SelectUsers = Yii::app()->db->createCommand('SELECT * FROM videocms_users WHERE user_login = :UserLogin AND user_pass = :UserPass');
+        $SelectUsers->bindValue(':UserLogin', $this->user_login, PDO::PARAM_STR);
+        $SelectUsers->bindValue(':UserPass', md5($this->user_pass), PDO::PARAM_STR);
+        $DataUsers = $SelectUsers->queryAll();
+
+        return $DataUsers;
+    }
+}
+
+?>
