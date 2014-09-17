c5865324dc32db1893e1c6e0a3ad19fafc633afe
Tomek <Tomek@Laptop>
2014-09-17 17:19:46 +0200

    Poprawienie adresu w menu do kategorii

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -62,7 +62,7 @@ $.noConflict();
 				array('label'=>'Home', 'url'=>array('/cmsvideo/index')),
 				array('label'=>'Wyloguj', 'url'=>array('/zaloguj/wyloguj')),
                             array('label'=>'Wpisy', 'url'=>array('/admin/wpisy')),
-				array('label'=>'Kategorie', 'url'=>array('/admin/kategorie')),
+				array('label'=>'Kategorie', 'url'=>array('/admin/category')),
 				//array('label'=>'komentarze', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Zmień hasło', 'url'=>array('/admin/haslo')),
 			),
