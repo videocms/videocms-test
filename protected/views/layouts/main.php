ee67d880bed3cddeb7a67806ec5286bdb8ef1e8d
mykyy <mykyy@mykyy-desktop>
2014-09-20 15:47:27 +0200

    add tinymce

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -44,8 +44,23 @@ $.noConflict();
 </script> -->
         
         <!-- koniec video js -->
-	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/cke/ckeditor.js"></script>
-</head>
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/tinymce/tinymce.min.js"></script>
+<script>tinymce.init({
+         selector: "textarea",theme: "modern",width: 980,height: 500,
+    plugins: [
+         "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+         "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
+         "table contextmenu directionality emoticons paste textcolor responsivefilemanager code"
+   ],
+   toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
+   toolbar2: "| responsivefilemanager | link unlink anchor | image media | forecolor backcolor  | print preview code ",
+   image_advtab: true,
+   
+   external_filemanager_path:"/filemanager/",
+   filemanager_title:"Responsive Filemanager" ,
+   external_plugins: { "filemanager" : "/filemanager/plugin.min.js"}
+        });</script>
+        </head>
 
 <body>
 
