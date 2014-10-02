60aee027c2062c27e0c2f7c340b9787c8bb775c4
mykyy <mykyy@mykyy-desktop>
2014-10-02 14:40:12 +0200

    slider

--- protected/views/cmsvideo/index.php
+++ protected/views/cmsvideo/index.php
@@ -1,3 +1,68 @@
+<div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 600px; height: 300px;">
+    <!-- Slides Container -->
+    <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 600px; height: 300px;
+            overflow: hidden;">
+        <div>
+                <img u="image" src="http://jimdo.wpengine.com/wp-content/uploads/2014/01/tree-247122.jpg" />
+                <div u="caption" t="MCLIP|B" style="position: absolute; top: 250px; left: 0px;
+                    width: 600px; height: 50px;">
+                    <div style="position: absolute; top: 0px; left: 0px; width: 600px; height: 50px;
+                        background-color: Black; opacity: 0.5; filter: alpha(opacity=50);">
+                    </div>
+                    <div style="position: absolute; top: 0px; left: 0px; width: 600px; height: 50px;
+                        color: White; font-size: 16px; font-weight: bold; line-height: 50px; text-align: center;">
+                        <a href="http://www.w3schools.com">Visit W3Schools</a>
+                    </div>
+                </div>
+            </div>
+        <div>
+                <img u="image" src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" />
+                <div u="caption" t="MCLIP|B" style="position: absolute; top: 250px; left: 0px;
+                    width: 600px; height: 50px;">
+                    <div style="position: absolute; top: 0px; left: 0px; width: 600px; height: 50px;
+                        background-color: Black; opacity: 0.5; filter: alpha(opacity=50);">
+                    </div>
+                    <div style="position: absolute; top: 0px; left: 0px; width: 600px; height: 50px;
+                        color: White; font-size: 16px; font-weight: bold; line-height: 50px; text-align: center;">
+                        TOMEK!!!!!!! :D
+                    </div>
+                </div>
+            </div>
+    </div>
+     <style>
+            /* jssor slider arrow navigator skin 03 css */
+            /*
+            .jssora03l              (normal)
+            .jssora03r              (normal)
+            .jssora03l:hover        (normal mouseover)
+            .jssora03r:hover        (normal mouseover)
+            .jssora03ldn            (mousedown)
+            .jssora03rdn            (mousedown)
+            */
+            .jssora14l, .jssora14r, .jssora14ldn, .jssora14rdn
+            {
+            	position: absolute;
+            	cursor: pointer;
+            	display: block;
+                background: url(<?php echo Yii::app()->request->baseUrl; ?>/js/slider-jquery/img/a14.png) no-repeat;
+                overflow:hidden;
+            }
+            .jssora14l { background-position: -15px -35px; }
+            .jssora14r { background-position: -75px -35px; }
+            .jssora14l:hover { background-position: -135px -35px; }
+            .jssora14r:hover { background-position: -195px -35px; }
+            .jssora14ldn { background-position: -255px -35px; }
+            .jssora14rdn { background-position: -315px -35px; }
+        </style>
+        <!-- Arrow Left -->
+        <span u="arrowleft" class="jssora14l" style="width: 30px; height: 50px; top: 123px; left: 0px;">
+        </span>
+        <!-- Arrow Right -->
+        <span u="arrowright" class="jssora14r" style="width: 30px; height: 50px; top: 123px; right: 0px">
+        </span>
+        <!-- Arrow Navigator Skin End -->
+</div>
+
 <?php
 
 foreach ($DataCategory as $ModelCategoryShow)
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -121,6 +121,79 @@ $.noConflict();
    external_plugins: { "filemanager" : "/filemanager/plugin.min.js"}
         });</script>
 
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/slider-jquery/js/jssor.slider.min.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/slider-jquery/js/jssor.js"></script>
+        <!-- Caption Style -->
+    <style> 
+        .captionOrange, .captionBlack
+        {
+            color: #fff;
+            font-size: 20px;
+            line-height: 30px;
+            text-align: center;
+            border-radius: 4px;
+        }
+        .captionOrange
+        {
+            background: #EB5100;
+            background-color: rgba(235, 81, 0, 0.6);
+        }
+        .captionBlack
+        {
+        	font-size:16px;
+            background: #000;
+            background-color: rgba(0, 0, 0, 0.4);
+        }
+        a.captionOrange, A.captionOrange:active, A.captionOrange:visited
+        {
+        	color: #ffffff;
+        	text-decoration: none;
+        }
+        a.captionOrange:hover
+        {
+            color: #eb5100;
+            text-decoration: underline;
+            background-color: #eeeeee;
+            background-color: rgba(238, 238, 238, 0.7);
+        }
+        .bricon
+        {
+            background: url(<?php echo Yii::app()->request->baseUrl; ?>/js/slider-jquery/img/browser-icons.png);
+        }
+    </style>
+<script>
+    jQuery(document).ready(function ($) {
+        var _CaptionTransitions = [];
+            _CaptionTransitions["L"] = { $Duration: 800, x: 0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+            _CaptionTransitions["R"] = { $Duration: 800, x: -0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+            _CaptionTransitions["T"] = { $Duration: 800, y: 0.6, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+            _CaptionTransitions["B"] = { $Duration: 800, y: -0.6, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+            _CaptionTransitions["TL"] = { $Duration: 800, x: 0.6, y: 0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine, $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+            _CaptionTransitions["TR"] = { $Duration: 800, x: -0.6, y: 0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine, $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+            _CaptionTransitions["BL"] = { $Duration: 800, x: 0.6, y: -0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine, $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+            _CaptionTransitions["BR"] = { $Duration: 800, x: -0.6, y: -0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine, $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+
+            _CaptionTransitions["WAVE|L"] = { $Duration: 1500, x: 0.6, y: 0.3, $Easing: { $Left: $JssorEasing$.$EaseLinear, $Top: $JssorEasing$.$EaseInWave }, $Opacity: 2, $Round: { $Top: 2.5} };
+            _CaptionTransitions["MCLIP|B"] = { $Duration: 600, $Clip: 8, $Move: true, $Easing: $JssorEasing$.$EaseOutExpo };
+
+        var options = { 
+            $AutoPlay: true,
+            $ArrowNavigatorOptions: {                       //[Optional] Options to specify and enable arrow navigator or not
+                    $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
+                    $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                    $AutoCenter: 0,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                    $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+                },
+            $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
+                    $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
+                    $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
+                    $PlayInMode: 1,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
+                    $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
+                }
+            };
+        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+    });
+</script>
         </head>
 
 <body>
