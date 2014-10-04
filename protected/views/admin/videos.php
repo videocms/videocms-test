8926d5589cebe092338ccec5d1de2f171627ec08
Tomek <Tomek@Laptop>
2014-10-04 21:05:03 +0200

    bugs

--- protected/views/admin/videos.php
+++ protected/views/admin/videos.php
@@ -1,5 +1,10 @@
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Wideo</h1>
+    </div>
+</div>
+
 <?php
-echo '<h1>Wideo</h1>';
 echo '<table class="table table-hover" id="lol">';
 echo '<tr class="active">';
 echo '<th>Thumb</th>';
@@ -69,7 +74,6 @@ $this->widget('CLinkPager', array(
 ?>
 
 <div class="form">
-    <h2 class="admin">Dodaj nowe wideo</h2>
     <?php 
     $form=$this->beginWidget('CActiveForm', array(
         'id'=>'videocms-video-form',
@@ -87,74 +91,87 @@ $this->widget('CLinkPager', array(
             echo '<div class="alert alert-success" role="alert">Nowe wideo zostało dodane!</div>';
         }
         ?>
-        <div class="col-xs-12 col-md-8">
+        <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Dodaj nowe wideo</div>
+            <div class="panel-body">
+            <div class="row">
+            <div class="col-lg-6">
+                <div class="form-group">
             <?php echo $form->labelEx($ModelVideo, 'video_title'); ?>
             <?php echo $form->textField($ModelVideo, 'video_title', array('size' => 60, 'maxlength' => 65, 'class' => 'form-control', 'placeholder' => 'Tytuł')); ?>
             <?php echo $form->error($ModelVideo, 'video_title'); ?>
-            <div class="row">
+                </div>
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_text'); ?>
                 <?php echo $form->textArea($ModelVideo, 'video_text'); ?>
                 <?php echo $form->error($ModelVideo, 'video_text'); ?>
             </div>
-            <div class="row">
-                <p class="note">Pola oznacone <span class="required">*</span> są wymagane.</p>
             </div>
-        </div>
-        <div class="col-xs-6 col-md-4">
+           <div class="col-lg-6">
+               <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_alias'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_alias', array('class' => 'form-control', 'placeholder' => 'Alias', 'readonly' => true)); ?>
                 <?php echo $form->error($ModelVideo, 'video_alias'); ?>
-             <div class="row">
+               </div>
+             <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_480p'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_480p', array('class' => 'form-control', 'placeholder' => 'Link do rozdzielczości 480p')); ?>
                 <?php echo $form->error($ModelVideo, 'video_480p'); ?>  
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_720p'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_720p', array('class' => 'form-control', 'placeholder' => 'Link do rozdzielczości 720p')); ?>
                 <?php echo $form->error($ModelVideo, 'video_720p'); ?>  
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_1080p'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_1080p', array('class' => 'form-control', 'placeholder' => 'Link do rozdzielczości 1080p')); ?>
                 <?php echo $form->error($ModelVideo, 'video_1080p'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_category'); ?>
                 <?php echo $form->dropDownList($ModelVideo, 'video_category', CHtml::listData($ModelCategories->DownloadCategories(), 'category_id', 'category_name'), array('class' => 'form-control')); ?>
                 <?php echo $form->error($ModelVideo, 'video_category'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'player_type'); ?>
                 <?php echo $form->dropDownList($ModelVideo, 'player_type', array('video/mp4' => 'mp4', 'video/webm' => 'webm', 'video/ogg' => 'ogg', 'rtmp/mp4' => 'rtmp'),array('class' => 'form-control'));?>
                 <?php echo $form->error($ModelVideo, 'player_type'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php if ($ImageAdd) {echo '<div>Plik został wgrany na serwer</div>';} ?>
                 <?php echo $form->labelEx($ModelVideo, 'video_image'); ?>
                 <?php echo $form->fileField($ModelVideo, 'video_image'); ?>
                 <?php echo $form->error($ModelVideo, 'video_image'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_description'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_description', array('class' => 'form-control', 'placeholder' => 'Opis wideo')); ?>
                 <?php echo $form->error($ModelVideo, 'video_description'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_keywords'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_keywords', array('class' => 'form-control', 'placeholder' => 'Słowa kluczowe')); ?>
                 <?php echo $form->error($ModelVideo, 'video_keywords'); ?>
             </div>
-             <div class="row">
+             <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'tag_name'); ?>
                 <?php echo $form->textField($ModelVideo, 'tag_name', array('class' => 'form-control', 'placeholder' => 'Tagi')); ?>
                 <?php echo $form->error($ModelVideo, 'tag_name'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->dropDownList($ModelVideo, 'video_published',array('1' => 'Opublikowano', '0' => 'Nie opublikowano'),array('options' => array('1' => array('selected' => 'selected')), 'class' => 'form-control')); ?>            </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo CHtml::submitButton('Dodaj', array('class' => 'btn btn-success')); ?>
             </div>
+            <div class="form-group">
+                <p class="note">Pola oznacone <span class="required">*</span> są wymagane.</p>
+            </div>
+        </div>
+        </div>
+        </div>
+        </div>
         </div>
     </div>
 </div>
--- protected/views/admin/videoupdate.php
+++ protected/views/admin/videoupdate.php
@@ -1,5 +1,9 @@
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Edytuj wideo</h1>
+    </div>
+</div>
 <div class="form">
-    <h2>Edycja wpisu</h2>
     <?php 
     $form=$this->beginWidget('CActiveForm', array(
         'id'=>'videocms-videos-form',
@@ -17,51 +21,54 @@
             echo '<div class="alert alert-success" role="alert">Wideo zostało zaktualizowane!</div>';
         }
         ?>
-        <div class="col-xs-12 col-md-8">
+        <div class="col-lg-12">
+             <div class="panel panel-default">
+                 <div class="panel-heading"><?php echo '<b>'.$ModelVideo->video_title.'</b>'; ?></div>
+                 <div class="panel-body">
+                       <div class="col-lg-6">
+           <div class="form-group">
             <?php echo $form->labelEx($ModelVideo, 'video_title'); ?>
             <?php echo $form->textField($ModelVideo, 'video_title', array('size' => 60, 'maxlength' => 65, 'class' => 'form-control', 'placeholder' => 'Tytuł')); ?>
             <?php echo $form->error($ModelVideo, 'video_title'); ?>
-            <div class="row">
+            </div>
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_text'); ?>
                 <?php echo $form->textArea($ModelVideo, 'video_text'); ?>
                 <?php echo $form->error($ModelVideo, 'video_text'); ?>
             </div>
-            <div class="row">
-                <p class="note">Pola oznacone <span class="required">*</span> są wymagane.</p>
             </div>
-        </div>
-   
- 
-     <div class="col-xs-6 col-md-4">
+      <div class="col-lg-6">
+          <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_alias'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_alias', array('class' => 'form-control', 'placeholder' => 'Alias', 'readonly' => true)); ?>
                 <?php echo $form->error($ModelVideo, 'video_alias'); ?>
-             <div class="row">
+          </div>
+           <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_480p'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_480p', array('class' => 'form-control', 'placeholder' => 'Link do rozdzielczości 480p')); ?>
                 <?php echo $form->error($ModelVideo, 'video_480p'); ?>  
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_720p'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_720p', array('class' => 'form-control', 'placeholder' => 'Link do rozdzielczości 720p')); ?>
                 <?php echo $form->error($ModelVideo, 'video_720p'); ?>  
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_1080p'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_1080p', array('class' => 'form-control', 'placeholder' => 'Link do rozdzielczości 1080p')); ?>
                 <?php echo $form->error($ModelVideo, 'video_1080p'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_category'); ?>
                 <?php echo $form->dropDownList($ModelVideo, 'video_category', CHtml::listData($ModelCategories->DownloadCategories(), 'category_id', 'category_name'), array('class' => 'form-control')); ?>
                 <?php echo $form->error($ModelVideo, 'video_category'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'player_type'); ?>
                 <?php echo $form->dropDownList($ModelVideo, 'player_type', array('video/mp4' => 'mp4', 'video/webm' => 'webm', 'video/ogg' => 'ogg', 'rtmp/mp4' => 'rtmp'),array('class' => 'form-control'));?>
                 <?php echo $form->error($ModelVideo, 'player_type'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_image'); ?>
                 <p id="upload">
                 <div class="col-xs-10">
@@ -70,26 +77,26 @@
                 <input class="btn btn-default btn-sm" name="upload1" type="button" value="zmien" onclick="changeMode()" /></p>       
                  <?php echo $form->error($ModelVideo, 'video_image'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->textField($ModelVideo, 'video_thumb',array('hidden'=>true)); ?>
                 <?php echo $form->error($ModelVideo, 'video_thumb'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_description'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_description', array('class' => 'form-control', 'placeholder' => 'Opis wideo')); ?>
                 <?php echo $form->error($ModelVideo, 'video_description'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'video_keywords'); ?>
                 <?php echo $form->textField($ModelVideo, 'video_keywords', array('class' => 'form-control', 'placeholder' => 'Słowa kluczowe')); ?>
                 <?php echo $form->error($ModelVideo, 'video_keywords'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->labelEx($ModelVideo, 'tag_name'); ?>
                 <?php echo $form->textField($ModelVideo, 'tag_name', array('class' => 'form-control', 'placeholder' => 'Tagi')); ?>
                 <?php echo $form->error($ModelVideo, 'tag_name'); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php
                  $tagi = $ModelTags->DownloadTag($ModelVideo->video_id);
                 foreach($tagi as $data) { ?>
@@ -98,15 +105,19 @@
                     <label class="form-control"><?php echo $data['tag_name'];?></label></div>
                 <?php } ?>
             </div>    
-            <div class="row">       
+            <div class="form-group">      
                 <?php echo $form->hiddenField($ModelVideo, 'tag_delete', array('type'=>"hidden")); ?>
             </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo $form->dropDownList($ModelVideo, 'video_published',array('1' => 'Opublikowano', '0' => 'Nie opublikowano'),array('options' => array('1' => array('selected' => 'selected')), 'class' => 'form-control')); ?>            </div>
-            <div class="row">
+            <div class="form-group">
                 <?php echo CHtml::submitButton('Aktualizuj', array('class' => 'btn btn-success')); ?>
                 <?php echo CHtml::link('Anuluj',array('admin/videos'),array('class'=>'btn btn-danger')); ?>
             </div>
+    </div>
+    </div>
+    </div>
+    </div>
     </div>
 </div>
     
