969acfe3abb432b1be091dfd1f7b8c7eb09fca9a
mykyy <mykyy@mykyy-desktop>
2014-10-06 13:53:21 +0200

    fix

--- protected/views/admin/vast.php
+++ protected/views/admin/vast.php
@@ -6,7 +6,19 @@
 <?php
 $this->widget('zii.widgets.grid.CGridView', array(
 'id' => 'videocms-category-grid',
+'htmlOptions'=>array('class'=>'table-responsive'),
 'dataProvider'=>$Data,
+//'pager'=>array( 
+//   'cssFile'=>false,   
+        // 'header'=>'testttt',           
+//    'maxButtonCount'=>'7',
+//    'selectedPageCssClass'=>'active',
+//    'htmlOptions'=>array(
+//            'class'=>'pagination'
+//        ),
+//    ),
+'pagerCssClass'=>'pagination',
+'itemsCssClass'=>'table table-hover',   
 'columns'=>array(
         'vast_id',
         'vast_title',
