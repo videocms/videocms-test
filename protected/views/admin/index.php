36961cce62054b71c04372a12b0bd59e58fb5706
mykyy <mykyy@mykyy-desktop>
2014-10-08 18:35:27 +0200

    fix

--- protected/views/admin/index.php
+++ protected/views/admin/index.php
@@ -10,7 +10,8 @@ $xml = simpleXML_load_file($url,"SimpleXMLElement",LIBXML_NOCDATA);
     <div class="col-lg-12">
         <h1 class="page-header">Pulpit</h1>
     </div>
-</div>   
+</div> 
+<div class="row">
 <?php
 if($xml ===  FALSE) 
 { 
@@ -19,7 +20,6 @@ if($xml ===  FALSE)
 else { 
      if ($xml->wer == $wers) {
 ?>
-<div class="row">
     <div class="col-lg-3 col-md-6">
         <div class="panel panel-primary">
             <div class="panel-heading">
@@ -42,13 +42,11 @@ else {
             </div>
         </div>
     </div>
-</div>
 <?php
 }
    else
         {
-       echo '<div class="row">
-    <div class="col-lg-3 col-md-6">
+       echo '<div class="col-lg-3 col-md-6">
         <div class="panel panel-danger">
             <div class="panel-heading">
                 <div class="row">
@@ -69,8 +67,7 @@ else {
                 <div class="clearfix"></div>
             </div>
         </div>
-    </div>
-</div>';
+    </div>';
            // echo "<font color='red'>Najnowsza wersja to: <b>".$xml->wer."</font></b>";
     }  
         
@@ -78,3 +75,25 @@ else {
 
 
 ?>
+<div class="col-lg-4">
+    <div class="panel panel-primary">
+        <div class="panel-heading">CZAT</div>
+        <div class="panel-body"><p>
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'videocms-test'; // required: replace example with your forum shortname
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        </p>
+        </div>
+    </div>
+</div>
+</div>
