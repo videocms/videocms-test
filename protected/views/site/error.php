89dee1ee5190a99d13118c38e0c7a17b7c5d5eac
mykyy <mykyy@mykyy-desktop>
2014-10-08 15:45:40 +0200

    fix

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -7,9 +7,26 @@ $this->breadcrumbs=array(
 	'Error',
 );
 ?>
-
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-<?php echo CHtml::encode($message); ?>
+<div class="container">
+        <div class="col-lg-8 col-lg-offset-2 text-center">
+	  <div class="logo">
+	    <h1>OPPS, <?php echo $code; ?></h1>
+          </div><br /><br />
+            <div class="clearfix"></div>
+            <br /><br />
+            <div class="alert alert-danger" role="alert"><?php echo CHtml::encode($message); ?></div>
+          <div class="clearfix"></div>
+             <br /><br />
+            <div class="clearfix"></div>
+            <br />
+                <div class="col-lg-6  col-lg-offset-3">
+		  <div class="btn-group btn-group-justified">
+		      <a href="javascript:history.back()" class="btn btn-primary">Cofnij</a>
+                      <a href="/" class="btn btn-success">Strona główna</a> 
+		  </div>
+                        
+                </div>
+            
+        </div>
+              
 </div>
