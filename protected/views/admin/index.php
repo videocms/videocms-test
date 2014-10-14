ee94f8c8bcc3bc8f8a711c902b31740fbe3fd060
Tomek <Tomek@Laptop>
2014-10-14 15:57:33 +0200

    zmiany

--- protected/views/admin/index.php
+++ protected/views/admin/index.php
@@ -19,30 +19,6 @@ Yii::app()->db->createCommand()->update(
 //$test = implode(",",$new);
 //echo $test;
 
-$this->widget('zii.widgets.grid.CGridView', array(
-'id' => 'videocms-sess-grid',
-'htmlOptions'=>array('class'=>'table-responsive'),
-'summaryCssClass' => 'dataTables_info',
-'template' => '{items}',
-'dataProvider'=>$Data,
-'pager'=>array( 
-   'cssFile'=>false,   
-    'header'=>'',  
-    'maxButtonCount'=>'7',
-    'selectedPageCssClass'=>'active',
-   'htmlOptions'=>array(
-           'class'=>'pagination'
-       ),
-    ),
-'pagerCssClass'=>'pagination',
-'itemsCssClass'=>'table table-hover',    
-'columns'=>array(
-        'username',
-        'user_ip',
-    ),
-));
-
-
 
 $wers = "0.3";
 $url = 'http://www.alexie.pl/wersja.xml'; 
@@ -117,26 +93,74 @@ else {
 
 
 ?>
-<div class="col-lg-4">
-    <div class="panel panel-primary">
-        <div class="panel-heading">CZAT</div>
-        <div class="panel-body"><p>
-    <div id="disqus_thread"></div>
-    <script type="text/javascript">
-        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'videocms-test'; // required: replace example with your forum shortname
-
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-        </p>
-        </div>
-    </div>
+    <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-tasks fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">12</div>
+                                    <div>New Tasks!</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 </div>
+<div class="row">
+    <div class="col-lg-8">
+         <div class="panel panel-primary">
+            <div class="panel-heading">CZAT</div>
+            <div class="panel-body"><p>
+                <div id="disqus_thread"></div>
+                <script type="text/javascript">
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'videocms-test'; // required: replace example with your forum shortname
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                    </p>
+            </div>
+    </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-users fa-fw"></i> Zalogowani użytkownicy
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="list-group">
+                                <?php 
+                                $activeUsers = Session::model()->findAll();
+                                foreach($activeUsers as $Data) {
+                                echo '<a href="#" class="list-group-item">
+                                    <i class="fa fa-user fa-fw"></i> '.$Data->username.'
+                                    <span class="pull-right text-muted small"><em>'.$Data->user_ip.'</em>
+                                    </span>
+                                     </a>';
+                                 }
+                                ?>
+                            </div>
+                            <a href="admin/users" class="btn btn-default btn-block">Zarządzaj użytkownikami</a>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+    </div>
 </div>
 <?php echo 'Twój adres IP to: '.Yii::app()->request->userHostAddress;?> 
