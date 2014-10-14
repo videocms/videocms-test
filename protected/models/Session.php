66d9e976414338a7801ac19ec1dc9fb10a1a9164
Tomek <Tomek@Laptop>
2014-10-14 16:56:16 +0200

    zmiany

--- protected/models/Session.php
+++ protected/models/Session.php
@@ -19,6 +19,13 @@ class Session extends CActiveRecord
                );
         }
         
+    public function relations()
+        {
+          return array(
+                'user' => array(self::BELONGS_TO, 'User', 'username'),
+            );
+        }
+        
     public function attributeLabels() 
         {
             return array(
--- protected/models/User.php
+++ protected/models/User.php
@@ -119,17 +119,12 @@ class User extends CActiveRecord
 		);
 	}
 
-	/**
-	 * @return array relational rules.
-	 */
 	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		//	'posts' => array(self::HAS_MANY, 'Post', 'author_id'),
-		);
-	}
+        {
+          return array(
+                'session' => array(self::HAS_MANY, 'Session', 'username'),
+            );
+        }
 
 	/**
 	 * @return array customized attribute labels (name=>label)
