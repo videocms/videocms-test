0f6a49c5983a68ad1710753e39eaf7ea9ba0089c
mykyy <mykyy@mykyy-desktop>
2014-10-21 13:15:32 +0200

    likde&dislike

---
+++ protected/modules/likedislike-copy/controllers/DefaultController.php
@@ -0,0 +1,55 @@
+<?php
+
+class DefaultController extends Controller
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+	
+	public function actionLikedislike(){
+		$field_id = Yii::app()->request->getParam('field_id');
+		//$user_id = yii::app()->user->GetId();
+                $user_ip = Yii::app()->request->userHostAddress;
+		
+		$criteria=new CDbCriteria;
+		$criteria->select='*';  // only select the 'title' column
+                //$criteria->condition='field_id=:field_id and user_ip=:user_ip';
+		//$criteria->condition='field_id=:field_id and user_id=:user_id';
+		//$criteria->params=array(':field_id'=>$field_id,':user_id'=>$user_id);
+                $criteria->condition='field_id=:field_id and user_ip=:user_ip';
+		$criteria->params=array(':field_id'=>$field_id,':user_ip'=>$user_ip);
+		$model = Likedislike::model()->find($criteria);
+                //$model1 = Likedislike::model()->find($criteria);
+		
+		if(count($model)==0){
+			$model = new Likedislike();
+			$model->field_id = $field_id;
+			$model->user_ip = $user_ip;
+                        //$model->user_id = $user_id;
+			$model->status = 1;
+			$model->add_timestamp = time();
+			$displaynow = 'like';
+		}
+		else if($model->status==0){
+			$model->status = 1;
+			$model->edit_timestamp = time();
+			$displaynow = 'like';
+		}
+		else{
+			$model->status = 0;
+			$model->edit_timestamp = time();
+			$displaynow = 'like';
+		}
+		
+		if($model->save()){
+			$data['status'] = true;
+			$data['displaytext'] = $displaynow;
+		}
+		else{
+			$data['status'] = false;
+		}
+		$data['count'] = Yii::app()->getModule('likedislike')->countlikes($field_id);
+		echo json_encode($data);
+	}     
+}
